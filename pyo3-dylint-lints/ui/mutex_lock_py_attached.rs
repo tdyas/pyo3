cc07e480f5994b787285af29f3edc9c8cc4e6109
Tom Dyas <tom.dyas@gmail.com>
2025-10-11 21:29:16 -0700

    checkpoint

--- pyo3-dylint-lints/ui/mutex_lock_py_attached.rs
+++ pyo3-dylint-lints/ui/mutex_lock_py_attached.rs
@@ -2,16 +2,21 @@
 
 use pyo3::marker::Python;
 use pyo3::sync::MutexExt;
-use std::sync::Mutex;
+use std::sync::Mutex as StdMutex;
+use parking_lot::Mutex as PLMutex;
 
 // Should trigger the lint
-fn bad_example_basic(_py: Python<'_>, mutex: &Mutex<i32>) {
+fn bad_example_basic_std(_py: Python<'_>, mutex: &StdMutex<i32>) {
     let _guard = mutex.lock().unwrap();
+}
+
+fn bad_example_basic_parking_lot(_py: Python<'_>, mutex: &PLMutex<i32>) {
+    let _guard = mutex.lock();
 }
 
 // Should trigger the lint - mutex in struct
 struct MyStruct {
-    data: Mutex<String>,
+    data: StdMutex<String>,
 }
 
 fn bad_example_struct(_py: Python<'_>, s: &MyStruct) {
@@ -19,17 +24,17 @@ fn bad_example_struct(_py: Python<'_>, s: &MyStruct) {
 }
 
 // Should NOT trigger - no Python token
-fn ok_no_python_token(mutex: &Mutex<i32>) {
+fn ok_no_python_token(mutex: &StdMutex<i32>) {
     let _guard = mutex.lock().unwrap();
 }
 
 // Should NOT trigger - using lock_py_attached
-fn ok_using_lock_py_attached(py: Python<'_>, mutex: &Mutex<i32>) {
+fn ok_using_lock_py_attached(py: Python<'_>, mutex: &StdMutex<i32>) {
     let _guard = mutex.lock_py_attached(py).unwrap();
 }
 
 // Should NOT trigger - try_lock is fine
-fn ok_using_try_lock(_py: Python<'_>, mutex: &Mutex<i32>) {
+fn ok_using_try_lock(_py: Python<'_>, mutex: &StdMutex<i32>) {
     if let Ok(_guard) = mutex.try_lock() {
         // work with guard
     }
